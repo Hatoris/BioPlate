71199c92badd1a85163a36477768c2f3122eaf2e
Hatoris <florianxbernard@gmail.com>
2017-11-25 14:04:30 -0500

    start to create decorator for open do close db

--- plate_db.py
+++ plate_db.py
@@ -7,7 +7,8 @@ class create_plate_db:
 
     def __init__(self, DB):
         #try:
-        self.conn = sqlite3.connect(DB)
+        self.DB = DB
+        self.conn = self.connexion
         #except :
             #print(sys.exc_info()[0]) 
         self.c = self.conn.cursor()
@@ -16,6 +17,36 @@ class create_plate_db:
     @property
     def close(self):
         return self.conn.close()
+        
+    @property
+    def connexion(self):
+        return sqlite3.connect(self.DB)
+        
+    @property
+    def cursor(self):
+        return self.conn.cursor()
+        
+    def open_close(connexion=False, cursor=False):
+        def pass_func(func):
+            def pass_self(self, func):
+                def wrapper(self, *args, **kwargs):
+                    conn = self.connexion
+                    if connexion:
+                        print("connexion")
+                    elif cursor:
+                        print("cursor")
+                    else:
+                        print("none of this") 
+                    val = func(self, conn, *args) 
+                    conn.close
+                    print("close")
+                    return val
+                return wrapper
+            return pass_self
+        return pass_func
+
+        
+        
     
     def create_table(self):
         #self.conn
@@ -29,9 +60,11 @@ class create_plate_db:
         self.conn.commit()
         #self.close
         
-    def get_by_numWell(self, numWell):
-        self.c.execute('SELECT * FROM plates WHERE numWell=?', (numWell,)) 
-        return self.c.fetchone()
+    @open_close(cursor=True)
+    def get_by_numWell(self, conn, numWell):
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM plates WHERE numWell=?', (numWell,)) 
+        return cursor.fetchone() 
         
     def get_all(self):
         self.c.execute('SELECT * FROM plates')
@@ -64,13 +97,14 @@ if __name__ == "__main__":
     #Plates.add_value((6, 3, 2, 9.5, 2000, 2000,  ' https://www.google.ca/url?sa=t&source=web&rct=j&url=http://csmedia2.corning.com/LifeSciences/Media/pdf/cc_surface_areas.pdf&ved=0ahUKEwiEueSp8tnXAhXySd8KHd_ECXgQFgg1MAA&usg=AOvVaw2X9oIuhZs3izCw7OmvQE_f'))
     #print(Plates.get_by_numWell(6))
     #Plates.delete_by_id(2)
-    print(Plates.get_all())
+    #print(Plates.get_all())
     #print(Plates.get_column_name())
     #Plates.add_column_text("refURL")
-    plate = Plates.get_dict(Plates.get_by_numWell(96))
-    print(plate['numWell'], plate['refURL']) 
+    #plate = Plates.get_dict(Plates.get_by_numWell(96))
+    #print(plate['numWell'], plate['refURL']) 
     Plates.close
-    
+    print(Plates.get_by_numWell(6))
+    print(Plates.get_all())
         
         
         
