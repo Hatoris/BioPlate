2ce265c9ad7d02466958354d9de1cb5a05395f1d
Hatoris <florianxbernard@gmail.com>
2017-11-30 09:19:53 -0500

    remove db connexion and inherit it from general_db

--- database/plate_db.py
+++ database/plate_db.py
@@ -1,80 +1,36 @@
-import sqlite3
-import sys
-import os
-import re
+from general_db import general_db
 
 
 
-class create_plate_db:
-
-    def __init__(self, DB):
-        if re.search('database', sys.path[0]):
-            self.DB = os.path.join(sys.path[0], DB)
-        else :
-            self.DB = os.path.join(sys.path[0], 'database/' + DB) 
-        self.conn = self.connexion
-        self.c = self.conn.cursor()
-        self.create_table
-      
-    @property
-    def close(self):
-        return self.conn.close()
-        
-    @property
-    def connexion(self):
-        return sqlite3.connect(self.DB)
-        
-    @property
-    def cursor(self):
-        return self.conn.cursor()
-        
-    def open_close(connexion=False, cursor=False, commit=False):
-        def pass_self(func):
-            def wrapper(self, *args, **kwargs):
-                conn = self.connexion
-                curs = conn.cursor()
-                if connexion:
-                    val = func(conn, *args)
-                elif cursor:
-                    val = func(curs, *args)
-                elif commit:
-                    val = func(curs, *args)
-                    conn.commit() 
-                else:
-                    print("none of this") 
-                conn.close
-                return val
-            return wrapper
-        return pass_self
-
+class create_plate_db(general_db):
         
     @property    
-    @open_close(commit=True)
+    @general_db.open_close(commit=True)
     def create_table(cursor):
         cursor.execute("CREATE TABLE IF NOT EXISTS plates (id integer primary key autoincrement, numWell integer, numColumns integer, numRows integer, surfWell real, volWell real, workVol real, refURL text)")
      
-    @open_close(commit=True)   	
+    @general_db.open_close(commit=True)   	
     def add_value(cursor, value):
         cursor.execute("INSERT INTO plates VALUES (Null,?,?,?,?,?,?,?) ", value) 
 
         
-    @open_close(cursor=True)
+    @general_db.open_close(cursor=True)
     def get_by_numWell(cursor, numWell):
         cursor.execute('SELECT * FROM plates WHERE numWell=?', (numWell,)) 
         return cursor.fetchone() 
     
     @property
-    @open_close(cursor=True)    
+    @general_db.open_close(cursor=True)    
     def get_all(cursor):
         cursor.execute('SELECT * FROM plates')
         return cursor.fetchall()
         
-    @open_close(commit=True)   
+    @general_db.open_close(commit=True)   
     def delete_by_id(cursor, id):
         cursor.execute('DELETE FROM plates WHERE id=?', (id,))
         
     @property
-    @open_close(cursor=True)
+    @general_db.open_close(cursor=True)
     def get_column_name(cursor):
         curs = cursor.execute('SELECT * FROM plates WHERE id=0')
         names = [description[0] for description in curs.description]
