cc384c58d59000f5bcf0e2a100d9f311529830a1
Hatoris <florianxbernard@gmail.com>
2018-12-04 08:56:42 -0500

    modify conf.py to show full realease number on docs

--- build_docs/conf.py
+++ build_docs/conf.py
@@ -75,7 +75,7 @@ author = 'Florian Bernard'
 # built documents.
 #
 # The short X.Y version.
-version = '0.1'
+version = '0.1.3'
 # The full version, including alpha/beta/rc tags.
 release = '0.1.3'
 
