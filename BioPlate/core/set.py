0f97efbbd822e47cd7963e4a936b319358db5511
Hatoris <florianxbernard@gmail.com>
2019-12-10 22:15:42 -0500

    Creatation of a module to assgined value on plate

--- BioPlate/core/set.py
+++ BioPlate/core/set.py
@@ -1,68 +1,60 @@
+"""This module implemented all functionality to set value on a BioPlate object. 
+
+To assigne a value on a plate, we need the coordinate represented by column and row value. 
+Several values can be assigned in a loop.  
+"""
+
+from typing import List,Generator,Dict, Union,Tuple, Yields
+
+from BioPlate.core.matrix import well_to_index
 
 
 
 
 class Set:
 
-    def __init__(self, bioplate, *elements, merge=False):
-        """Main entry point to assign value on plate 
-           
-        Parameters 
-        ----------
-          well : dict or str
-              - if dict, well must contain well identifier as key and value to assign as value.eg : {"A2" : "value", "A[3-6]" : 42} 
-              - if string, well is only a well identifier eg : "G5" 
 
-         value : list or str or int or float 
-             - if list, value should be presented with multiple well identifer "B-D[2-5]", ["value1", "value2", "value3"]
-
-        merge : bool (by default False) 
-            Value on well are not overide but added
-        Returns
-        -------
-         BioPlate : BioPlate
-             return instance of plate
-
-         Exemples
-         --------    
-         see :ref:`Set-values-on-plate`
-                 
-        """
+    def __init__(self, *elements, merge=False):
         self.bioplate = bioplate
-        self.well, self.value = self._args_analyse(*elements)
+        self.set_iterator = iterate_elements(*elements)
         self.merge = merge
 
+class OneSet:
 
-    def _args_analyse(self, *args):
-        """
+    def __init__(self, well, value, merge=False):
+        self.well = well 
+        self.value = value 
+        self.part = len(value) if isinstance(value, (list, tuple)) else 1
+        self.merge = merge 
 
-        Parameters
-        ----------
-        well : dict or str
-                   stand alone args with value for each well
-         value : list or str or int or float
-                      list of value or value alone
 
-        Returns
-        -------
-        well : dict or str
-                    well position
-         value : list or str or int or float or None
-                     value for given well, None if dict was pass as argument
-                
-        Examples
-        ----------
-        
-        >>> BioPlateManipulation._args_analyse({"A1" : "test"})
-        ({"A1" : "test"}, None)
-        >>> BioPlateManipulation._args_analyse("A[1-2]",  ["test", "test1"])
-        ("A[1-2]",  ["test", "test1"])
+    def merged_value(self, bioplate)
 
-        """
-        if len(args) == 1:
-            well, *trash = args
-            value = None
-            return well, value
-        if len(args) == 2:
-            well, value, *trash = args
-            return well, value
+
+def iterate_elements(*elements) -> Yields:
+    """Create a generator from well if its an iterable
+    
+    Parameters
+    ----------
+    well : Union[List, Tuple, Dict]
+        An iterable containg pairs of coordinate and value
+    
+    Yields
+    -------
+    Generator
+        yield a tuple of well and value to assigned
+    """
+    well_human, value = elements if len(elements) == 2 else elements, None 
+
+    if value is not None:
+        yield well_to_index(well_human), value
+    else:
+        generator = well_human.items() if isinstance(well_human, dict) else well_human
+        for well_human, value in generator:
+            yield well_to_index(well_human), value
+
+
+
+
+
+
